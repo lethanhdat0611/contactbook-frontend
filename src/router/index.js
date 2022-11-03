e153f87cbcd5ee85c9d2fce5749f2b2c13abc352
lethanhdat0611 <113762542+lethanhdat0611@users.noreply.github.com>
2022-11-03 23:18:30 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,14 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 ];
+    
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
